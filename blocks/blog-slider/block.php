f8f6749c50db0d7733b8224968bc7adf3bef7a0a
dzikri <nadhimulloh@gmail.com>
2025-03-07 23:44:57 +0700

    fix: swiper blog

--- blocks/blog-slider/block.php
+++ blocks/blog-slider/block.php
@@ -29,6 +29,7 @@ $classes .= get_global_block_classes();
 $style   = get_global_block_bg();
 $type    = get_field('type');
 $blogs   = get_field('blogs');
+$title   = get_field('title');
 
 ?>
 
@@ -37,13 +38,18 @@ $blogs   = get_field('blogs');
     <div class="container">
         <div class="grid grid-cols-12">
             <div class="col-span-12 <?php print $type == 'padded' ? 'lg:col-span-10 lg:col-start-2 2xl:col-span-8 2xl:col-start-3' : ''; ?>">
+                <?php if ($title): ?>
+                    <h2 class="mb-10"><?php print $title; ?></h2>
+                <?php endif; ?>
                 <?php if ($blogs): ?>
                     <div class="swiper-blog overflow-visible <?php print $type; ?>">
                         <div class="swiper-wrapper">
                             <?php foreach ($blogs as $item): ?>
                                 <div class="swiper-slide mr-5 last:mr-0 h-auto min-h-[450px] md:min-h-[480px] w-[300px] md:w-[320px] lg:w-[340px] flex flex-col overflow-hidden" item-fade-animate>
                                     <a href="<?php print get_permalink( $item->ID ); ?>">
-                                        <img src="<?php print get_the_post_thumbnail_url( $item->ID ); ?>" alt="<?php print $item->post_title; ?>" class="w-full h-[264px] md:h-[282px] lg:h-[300px] object-cover transition-all duration-200 hover:scale-110">
+                                        <div class="img-container overflow-hidden w-full h-[264px] md:h-[282px] lg:h-[300px]">
+                                            <img src="<?php print get_the_post_thumbnail_url( $item->ID ); ?>" alt="<?php print $item->post_title; ?>" class="object-cover transition-all duration-200 hover:scale-110">
+                                        </div>
                                         <div class="relative text-container grow z-10">
                                             <h4 class="text-[22px] md:text-[24px] lg:text-[26px] mb-[10px] text-blue"><?php print $item->post_title; ?></h4>
                                             <p class="p2 font-sans-alt text-dark-gray"><?php print get_the_excerpt( $item->ID ); ?></p>
