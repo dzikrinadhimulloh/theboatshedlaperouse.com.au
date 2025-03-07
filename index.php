f5afc033604586db9ae2a3ad3b4325dbd9035fa0
dzikri <nadhimulloh@gmail.com>
2025-03-07 22:51:51 +0700

    fix: overlay on hero

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <?php while (have_posts()) : the_post(); ?>
     <?php if ($thumbnail_image): ?>
-        <div class="relative hero bg-cover bg-center w-full overflow-hidden min-h-[720px] md:min-h-[1024px] lg:min-h-fit  <?php print is_front_page() ? 'lg:aspect-[18/13]' : 'lg:aspect-[36/19]'; ?>" style="background-image:url(<?php print $thumbnail_image?>)">
+        <div class="relative hero bg-cover bg-center w-full overflow-hidden min-h-[720px] md:min-h-[1024px] lg:min-h-fit bg-black/40 bg-blend-multiply <?php print is_front_page() ? 'lg:aspect-[18/13]' : 'lg:aspect-[36/19]'; ?>" style="background-image:url(<?php print $thumbnail_image?>)">
             <!-- <div class="top-parallax bg-black/40 bg-blend-multiply"></div> -->
             <div data-speed="2" class="animate-speed absolute text-center right-0 left-0 text-section <?php print is_front_page() ? 'top-1/2 -translate-y-2/4' : 'bottom-[100px] md:bottom-[140px]'; ?>">
                 <h1 class="max-w-[90%] md:max-w-[80%] lg:max-w-[960px] mx-auto text-white"><?php print get_the_post_thumbnail_caption( get_the_ID() ); ?></h1>
