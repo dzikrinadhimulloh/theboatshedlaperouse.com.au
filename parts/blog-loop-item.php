b50f4f174e2ec8cd8556f22ae9a95c388bf027b9
dzikri <nadhimulloh@gmail.com>
2025-03-17 19:55:40 +0700

    fix: styling

--- parts/blog-loop-item.php
+++ parts/blog-loop-item.php
@@ -8,7 +8,7 @@
             <a href="<?php print get_permalink( $item->ID ); ?>" class="flex flex-col lg:flex-row gap-6 lg:gap-10 border-b border-[#aaaaaa] my-10 first:mt-0 pb-10 ">
                 <div class="basis-full lg:basis-5/12">
                     <div class="rounded-[10px] aspect-[32/25] overflow-hidden">
-                        <img src="<?php print get_the_post_thumbnail_url( $item->ID ); ?>" alt="<?php print get_blog_thumb_alt_text($item); ?>" class="rounded-[10px] aspect-[32/25] hover:scale-110 transition-all duration-200">
+                        <img src="<?php print get_the_post_thumbnail_url( $item->ID ); ?>" alt="<?php print get_blog_thumb_alt_text($item); ?>" class="rounded-[10px] aspect-[32/25] hover:scale-110 transition-all duration-200 w-full">
                     </div>
                 </div>
                 <div class="basis-full lg:basis-7/12">
